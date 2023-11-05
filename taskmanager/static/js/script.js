d88b2f898a2ad1fdd6f4d22f25b77f8617a1ce69
yousefalm1 <>
2023-11-05 16:20:32 +0000

    Adding Tasks

--- taskmanager/static/js/script.js
+++ taskmanager/static/js/script.js
@@ -2,4 +2,16 @@ document.addEventListener('DOMContentLoaded', function () {
     // SideNav Initialization
     let sidenav = document.querySelectorAll('.sidenav');
     M.Sidenav.init(sidenav);
-});
+
+    // DatePicker Initialization
+    let datepicker = document.querySelectorAll('.datepicker');
+    M.Datepicker.init(datepicker, {
+        format: "dd mmmm yyyy",
+        i18n: {done: "Select"}
+
+    });
+
+    // select initialization
+    let selects = document.querySelectorAll("select");
+    M.FormSelect.init(selects);
+})
